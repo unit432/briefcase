346a6eed1676979632f960ab854bb7946511050e
Chen <cdong1@ualberta.ca>
2024-12-26 09:57:49 -0800

    disable line blame

--- VIM/neovim_init.lua
+++ VIM/neovim_init.lua
@@ -195,6 +195,7 @@ local plugins = {
           delay = 1000,
           ignore_whitespace = false,
         },
+        current_line_blame = false, -- Disable inline blame
         current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
         sign_priority = 6,
         update_debounce = 100,
